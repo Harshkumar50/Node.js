b1d45b8cb4c4aef31f3e14b0dfbb803743f1d7ce
Harsh kumar <115373712+Harshkumar50@users.noreply.github.com>
2024-02-12 13:16:34 +0530

    Create Arr1.js

---
+++ Arr1.js
@@ -0,0 +1,8 @@
+const subject=Array['javascript','nodejs','reactjs'];
+let arr1 =[11,12,13]
+let arr2 =[11,12,13]
+let arr3 =[11,12,13]
+console.log(arr1.concat(arr2,arr3));
+let number=[6,4,8,2,7,8,10,101,100, 'hello']
+let even = number.filter((x)=>(x%2==0))
+console.log(even)
